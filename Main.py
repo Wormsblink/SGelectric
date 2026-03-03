4997ae0be9c6a3cfb507464ec7844543ff16e834
Wormsblink <wormsblink@gmail.com>
2026-03-04 00:54:42 +0800

    v0.1.2
    change back to matplotlib for animations

--- Main.py
+++ Main.py
@@ -1,26 +1,65 @@
 import merge
 import pandas as pd
 from datetime import datetime
-from bokeh.plotting import figure, show
+
+import matplotlib
+from matplotlib import animation
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+from matplotlib.dates import DateFormatter
+
+#temp
+import time
 
 #Settings
 USEP_Folder = "USEP Data"
 USEP_Compiled_File = "Compiled USEP Data.csv"
 
-merge.merge_csv(USEP_Folder, USEP_Compiled_File)
+#plt.rcParams.update({'font.size': 16})
+#plt.rcParams['figure.figsize'] = 12.5, 7
 
-df = pd.read_csv(USEP_Compiled_File)
+def main():
+	merge.merge_csv(USEP_Folder, USEP_Compiled_File)
 
-x = []
-raw_dates = df['DATE'].values.tolist()
+	df = pd.read_csv(USEP_Compiled_File, usecols=["DATE","USEP ($/MWh)", "DEMAND (MW)"])
+	df = add_daily_average(df, "USEP ($/MWh)","DAILY USEP ($/MWh)")
 
-for date in raw_dates:
-	x.append(datetime.strptime(date,"%Y-%m-%d"))
+	#df.to_csv("test.csv")
 
-y1 = df['USEP ($/MWh)'].values.tolist()
+	animated_gif = create_image(df, "DATE", "DAILY USEP ($/MWh)","DEMAND (MW)")
+	animated_gif.show()
 
-plot = figure (title = "USEP over time", x_axis_label = "Date", x_axis_type ='datetime', y_axis_label = "($/MWh)")
+def add_daily_average(df, target_col, new_col_name):
+	df[new_col_name] = df[target_col].rolling(48).mean()
+	df = df.iloc[24::48, :]
+	return df
 
-plot.line(x, y1, legend_label="USEP", line_width=2)
+def create_image(df, date_col_name, *args):
 
-show(plot)
+	fig = plt.figure()
+	plt.xlabel(date_col_name)
+	fig.get_axes()[0].set_axis_off()
+
+	x = get_dates_list(df, "DATE")
+
+	for row_pos,dataset in enumerate(args):
+		newline = df[dataset].values.tolist()
+	
+		ax = fig.add_subplot(len(args),1,row_pos+1, sharex = fig.get_axes()[0])
+		ax.plot(x, newline, label = dataset)
+		ax.legend(loc = 'upper right')
+	
+	plt.tight_layout()
+
+	return plt
+
+def get_dates_list(df, date_col_name):
+	raw_dates = df[date_col_name].values.tolist()
+
+	parsed_dates = []
+
+	for date in raw_dates:
+		parsed_dates.append(datetime.strptime(date,"%Y-%m-%d"))
+	return parsed_dates
+
+main()			
